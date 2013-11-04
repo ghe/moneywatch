1676bb88021c9c0645696f69ab5a1211c2ef2f9c
Gerard Ens <GerardEns@gmail.com>
2013-11-03 20:53:37 -0700

    added time display

--- src/mainwidget.py
+++ src/mainwidget.py
@@ -16,21 +16,26 @@ from kivy.uix.widget import Widget
 from kivy.properties import (
     NumericProperty,
     BooleanProperty,
+    StringProperty,
 )
 from kivy.clock import Clock
 
+from datetime import timedelta
+
 class MainWidget(Widget):
-    wanna_quit = BooleanProperty(False)
     running = BooleanProperty(False)
     total = NumericProperty(0.00)
-    total_at_last_start = NumericProperty(0.00)
+    time_str = StringProperty("0:00:00")
     time_at_last_start = NumericProperty(0.00)
+    total_time = NumericProperty(0.00)
+    saved_time = NumericProperty(0.00)
     rate = NumericProperty(0.00)
 
     def update(self, dt):
        if (self.running):
-           run_time = Clock.get_boottime() - self.time_at_last_start
-           self.total = self.total_at_last_start + (run_time * (self.rate / 3600.0))
+           self.total_time = self.saved_time + Clock.get_boottime() - self.time_at_last_start
+           self.total = self.total_time * (self.rate / 3600.0)
+           self.time_str = str(timedelta(seconds=int(self.total_time)))
 
     def on_btn_start(self, str_rate):
         try:
@@ -41,15 +46,13 @@ class MainWidget(Widget):
         self.rate = rate
         self.running = True
         self.time_at_last_start = Clock.get_boottime()
-        self.total_at_last_start = self.total
 
     def on_btn_stop(self):
         self.running = False
+        self.saved_time = self.total_time
 
     def on_btn_reset(self):
         self.time_at_last_start = Clock.get_boottime()
-        self.total_at_last_start = 0
+        self.saved_time = 0.0
+        self.time_str = "0:00:00"
         self.total = 0
-
-    def on_btn_exit(self):
-        wanna_quit = True
